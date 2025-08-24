24cb28c3884924f2ba4c31aaa461a83294537c88
routAshutoshhh <egoisticdev@gmail.com>
2025-08-24 19:35:26 +0530

    model-validato and field-validator  initialised

---
+++ 03.model.behaviour/Assignment/Booking-model-assign.py
@@ -0,0 +1,9 @@
+#create a booking model with the following attributes
+"""
+Fields
+user-id : int
+room-id :int
+nights :int(must be >= 1)
+rates-per-night:float
+#also add a computed field :total_amount = nights*rates-per-night
+"""
---
+++ 03.model.behaviour/codeExample/model-validator.py
@@ -0,0 +1,2 @@
+#here we are going to learn about the model validator and feild-validator
+from pydantic import BaseModel,Field
