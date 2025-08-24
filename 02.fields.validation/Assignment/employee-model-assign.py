f3b108c5498a3841fdad3bbd99e8678a87f7b89d
routAshutoshhh <egoisticdev@gmail.com>
2025-08-24 18:57:11 +0530

    task for employee model

---
+++ 02.fields.validation/Assignment/employee-model-assign.py
@@ -0,0 +1,9 @@
+#create a model with the following things and attributes
+"""
+1. employee model 
+2. it must have an id of int type
+3. it must have a name of string type with minimum length of 3 and maximum length of 50 
+4. it must have a department of string type with the minimum length - this should be optional attribute as any employee can be not assigned to any department
+5. the employee model must have a salary of float type with a minimum value >=100000
+"""
+
--- 02.fields.validation/codeExample/fields.example.py
+++ 02.fields.validation/codeExample/fields.example.py
@@ -1,1 +1,17 @@
 from pydantic import BaseModel #type:ignore
+from typing import Optional ,List ,Dict 
+
+#creating the cart model 
+class Cart_model(BaseModel):
+    user_id : int 
+    items: List[str]
+    quantity : List[Dict[str, int]]#array of the dictionary which has the name of the item and the quantity
+
+
+#creating the blog post model to explore the Blog_post_model
+class Blog_post_model(BaseModel):
+    title:str
+    content:str
+    image_url : Optional[str] = None #mnaking this an optional field
+
+
